9946af6a2d11a1b6cf2351ce46ba983468586927
Fabio Biondi <info@fabiobiondi.com>
2024-03-25 00:51:51 +0100

    remove youtube configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'img.youtube.com',
-        port: '',
-        // pathname: '/account123/**',
-      },
-    ],
-  },
 };
 
 export default nextConfig;
